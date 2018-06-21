60e989da4df7d574d33224c4570edf6748e54814
Yannik Inniger <yannikinniger@Yanniks-MacBook-Pro.local>
2018-06-21 16:56:23 +0200

    Add additonal error handling

--- src/main/kotlin/domain/Order.kt
+++ src/main/kotlin/domain/Order.kt
@@ -1,5 +1,7 @@
 package domain
 
+import java.util.regex.Pattern
+
 /**
  * @author Yannik Inniger
  */
@@ -10,14 +12,28 @@ data class Order(
         val dn: String,
         val quantity: Int,
         val price: Int,
-        val articleNumber: String
+        val articleNumber: String,
+        val mailAddress: String
 ) {
     init {
         if (!dn.toLowerCase().contains("dn")) {
             throw IllegalArgumentException("invalid dn")
-        }
-        if (quantity < 0) {
-            throw IllegalArgumentException("invalid dn")
+        } else if (quantity < 0) {
+            throw IllegalArgumentException("quantity must be positive")
+        } else if (!mailAddress.isEmailValid()) {
+            throw IllegalArgumentException("email is invalid")
         }
     }
+
+    private fun String.isEmailValid(): Boolean {
+        return Pattern.compile(
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+                        + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+                        + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
+        ).matcher(this).matches()
+    }
+
 }
