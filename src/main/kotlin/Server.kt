a67c00897fe27fe2a2e760e9ac2bd6ad401b38ef
Yannik Inniger <yannik.inniger@gmail.com>
2018-06-21 17:38:11 +0200

    Send mail on post request

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,19 +1,37 @@
 import com.beust.klaxon.Klaxon
 import com.beust.klaxon.KlaxonException
 import domain.Order
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import spark.kotlin.post
 
 val klaxon = Klaxon()
+val logger: Logger = LoggerFactory.getLogger("Server")
+val mailer = Mailer()
 
 fun main(args: Array<String>) {
-    post("/send-mail") {
+
+    post("/order") {
         val orderJson = request.body()
         try {
             val order = klaxon.parse<Order>(orderJson)
-            println(order)
+            var emailSent = false
+            if (order != null) {
+                emailSent = mailer.sendEmail(order.mailAddress, "Kopie Ihrer Bestellung", order)
+                response.status(200)
+                response.body(klaxon.toJsonString(order as Any))
+                response.type("application/json")
+            }
+            if (!emailSent) {
+                response.status(400)
+                response.body("could not parse order")
+            }
         } catch (e: KlaxonException) {
+            logger.warn("unable to parse order $orderJson")
             response.status(400)
             response.body("could not parse order")
         }
+        return@post response.body()
     }
+
 }
