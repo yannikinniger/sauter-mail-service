46f1eff5d8f81b2bc0302a3d99ce8f50cf875c03
Yannik Inniger <yannik.inniger@gmail.com>
2018-06-22 10:48:57 +0200

    Add email address to test cases

--- src/test/kotlin/domain/OrderTest.kt
+++ src/test/kotlin/domain/OrderTest.kt
@@ -13,13 +13,13 @@ internal class OrderTest {
     @Test(expected = IllegalArgumentException::class)
     fun shouldThrowExceptionOnNegativeQuantity() {
         val quantity = -1
-        Order(address, address, 1, "DN20", quantity, 1, "article")
+        Order(address, address, 1, "DN20", quantity, 1, "article", "test@test.com")
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun shouldThrowExceptionOnInvalidDn() {
         val dn = "test"
-        Order(address, address, 1, dn, 1, 1, "article")
+        Order(address, address, 1, dn, 1, 1, "article", "test@test.com")
     }
 
 }
