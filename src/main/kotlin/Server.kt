8d4f0d2de89bc0dee9fd75c0e54da475309eccd6
Yannik Inniger <yannik.inniger@gmail.com>
2018-06-22 10:55:41 +0200

    Fix credentials not passed

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -40,7 +40,7 @@ fun main(args: Array<String>) {
 private fun initMailer(): Mailer {
     val userName = System.getenv("ZOHO_USER_NAME")
     val password = System.getenv("ZOHO_PASSWORD")
-    if (userName == "" || password == "") {
+    if (userName == null || password == null) {
         System.err.println("missing arguments")
         System.exit(1)
     }
