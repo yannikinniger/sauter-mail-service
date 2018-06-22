16d4719a41daf279955b79ec33033a87e909bf94
Yannik Inniger <yannik.inniger@gmail.com>
2018-06-22 12:17:02 +0200

    Add server tests

--- src/test/kotlin/ServerKtTest.kt
+++ src/test/kotlin/ServerKtTest.kt
@@ -1,3 +1,38 @@
-import org.junit.Assert.*
+import com.beust.klaxon.Klaxon
+import com.despegar.http.client.PostMethod
+import com.despegar.sparkjava.test.SparkServer
+import domain.Address
+import domain.Order
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import spark.servlet.SparkApplication
 
-class ServerKtTest
+class OrderTestServer : SparkApplication {
+    override fun init() {
+        main(arrayOf())
+    }
+}
+
+val testServer: SparkServer<OrderTestServer> = SparkServer(OrderTestServer::class.java, 4567)
+
+class ServerKtTest {
+
+    private val mailAddress = System.getenv("ZOHO_USER_NAME")
+    private val address = Address("Test AG", null, "Teststrasse", "Test City", 1000)
+    private val order = Order(address, address, 1, "DN10", 1, 1, "article", mailAddress)
+    private val klaxon = Klaxon()
+
+    @Test
+    fun shouldPostOrder() {
+        val orderJson = klaxon.toJsonString(order as Any)
+
+        val post: PostMethod = testServer.post("/order", orderJson, false)
+        val httpResponse = testServer.execute(post)
+        assertEquals(httpResponse.code(), 200)
+
+        val returnedOrder = String(httpResponse.body())
+        val result = klaxon.parse<Order>(returnedOrder)
+        assertEquals(order, result)
+    }
+
+}
