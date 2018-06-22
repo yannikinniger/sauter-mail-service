9aa6a92407734b2d4cdac83636badf4a5ba1680c
Yannik Inniger <yannik.inniger@gmail.com>
2018-06-22 10:54:43 +0200

    Add abstraction to mailer test

--- src/test/kotlin/MailerTest.kt
+++ src/test/kotlin/MailerTest.kt
@@ -1,3 +1,6 @@
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
 import domain.Address
 import domain.Order
 import org.junit.Assert.*
@@ -9,11 +12,11 @@ import org.junit.Test
 class MailerTest {
 
     private val address = Address("Test AG", null, "Teststrasse", "Test City", 1000)
-    private val order = Order(address, address, 1, "DN10", 1, 1, "article")
+    private val order = Order(address, address, 1, "DN10", 1, 1, "article", "test@test.ch")
 
     @Test
     fun shouldThrowExceptionOnInvalidEmail() {
-        val mailer = Mailer()
+        val mailer = initMailer()
 
         try {
             val mailAddress = "invalid@mail"
@@ -30,10 +33,9 @@ class MailerTest {
 
     @Test
     fun shouldSendEmailWithoutError() {
-        val mailer = Mailer()
-        val mailAddress = "yannik.inniger@gmail.com"
+        val mailer = initMailer()
+        val mailAddress = System.getenv("ZOHO_USER_NAME")
         val result = mailer.sendEmail(mailAddress, "subject", order)
-
         assertTrue(result)
     }
 
