e64dc27be89df2900cdbc7dc04ec8093def0d1b2
Yannik Inniger <yannik.inniger@gmail.com>
2018-06-25 18:10:03 +0200

    Seperate templates for customer and company

--- src/main/kotlin/domain/MailTemplate.kt
+++ src/main/kotlin/domain/MailTemplate.kt
@@ -5,38 +5,65 @@ import org.celtric.kotlin.html.*
 /**
  * @author Yannik Inniger
  */
-fun fillTemplate(order: Order): String {
-    val tableStyle = mapOf("style" to "padding-right: 20px")
+fun fillCustomerTemplate(order: Order): String {
     return div {
         h2("Vielen Dank für Ihre Bestellung bei Sauter") +
-        p("Hier senden wir Ihnen eine Kopie Ihrer Bestellung:") +
-            table {
-            tr {
-                th(other = tableStyle){ "Rechnungsadresse" } +
-                th("Lieferadresse")
-            } +
-            tr {
-                td(other = tableStyle){ order.deliveryAddress.company } +
-                td(order.invoiceAddress.company)
-            } +
-            tr {
-                td(other = tableStyle){ order.deliveryAddress.street } +
-                td(order.invoiceAddress.street)
-            } +
-            tr {
-                td(other = tableStyle) { order.deliveryAddress.zip + " " + order.deliveryAddress.city } +
-                td(order.invoiceAddress.zip + " " + order.invoiceAddress.city)
-            }
-        } +
-        br() +
-        h4("Bestellte Produkte") +
-        p("${order.quantity}x Heizungsregler EQJW126F001") +
-        p("${order.quantity}x Aussentemperatur-Fühler EGT301F102") +
-        p("${order.quantity}x Vorlauftemperatur-Fühler EGT311F102") +
-        p("${order.quantity}x Normschema") +
-        div("${order.quantity}x ${order.articleNumber}") +
-        div("DN: ${order.dn}") +
-        div("KVS: ${order.kvs}") +
-        div("Preis: ${order.price}")
+                p("Hier senden wir Ihnen eine Kopie Ihrer Bestellung:") +
+                fillAddresses(order) +
+                fillOrder(order)
     }.render()
+}
+
+fun fillOrderTemplate(order: Order): String {
+    return div {
+        h2("Neue Bestellung bei HLK Components") +
+                fillAddresses(order) +
+                p("Email: ${order.mailAddress}") +
+                fillOrder(order)
+    }.render()
+}
+
+
+private fun fillOrder(order: Order): BlockElement {
+    return div {
+        h4("Bestellte Produkte") +
+                p("${order.quantity}x Heizungsregler EQJW126F001") +
+                p("${order.quantity}x Aussentemperatur-Fühler EGT301F102") +
+                p("${order.quantity}x Vorlauftemperatur-Fühler EGT311F102") +
+                p("${order.quantity}x Normschema") +
+                div("${order.quantity}x ${order.articleNumber}") +
+                div("DN: ${order.dn}") +
+                div("KVS: ${order.kvs}") +
+                div("Preis: ${order.price}")
+    }
+}
+
+private fun fillAddresses(order: Order): BlockElement {
+    val tableStyle = mapOf("style" to "padding-left: 20px")
+    return table {
+        tr {
+            th("Rechnungsadresse") +
+                    th(other = tableStyle) { "Lieferadresse" }
+        } +
+                tr {
+                    td(order.deliveryAddress.company) +
+                            td(other = tableStyle) { order.invoiceAddress.company }
+                } +
+                if (order.deliveryAddress.project != null || order.invoiceAddress.project != null) {
+                    tr {
+                        td(order.deliveryAddress.project ?: "") +
+                                td(other = tableStyle) { order.invoiceAddress.project ?: "" }
+                    }
+                } else {
+                    span {  }
+                } +
+                tr {
+                    td(order.deliveryAddress.street) +
+                            td(other = tableStyle) { order.invoiceAddress.street }
+                } +
+                tr {
+                    td(order.deliveryAddress.zip + " " + order.deliveryAddress.city) +
+                            td(other = tableStyle) { order.invoiceAddress.zip + " " + order.invoiceAddress.city }
+                }
+    }
 }
