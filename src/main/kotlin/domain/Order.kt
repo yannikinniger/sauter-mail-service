548fb88e78968eb81a2063c1a4e21c30480eeded
Yannik Inniger <yannik.inniger@gmail.com>
2018-06-25 15:22:25 +0200

    Add send copy field

--- src/main/kotlin/domain/Order.kt
+++ src/main/kotlin/domain/Order.kt
@@ -13,27 +13,15 @@ data class Order(
         val quantity: Int,
         val price: Int,
         val articleNumber: String,
-        val mailAddress: String
+        val mailAddress: String,
+        val sendCopy: Boolean
 ) {
     init {
         if (!dn.toLowerCase().contains("dn")) {
             throw IllegalArgumentException("invalid dn")
         } else if (quantity < 0) {
             throw IllegalArgumentException("quantity must be positive")
-        } else if (!mailAddress.isEmailValid()) {
-            throw IllegalArgumentException("email is invalid")
         }
-    }
-
-    private fun String.isEmailValid(): Boolean {
-        return Pattern.compile(
-                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
-                        + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
-                        + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
-                        + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
-        ).matcher(this).matches()
     }
 
 }
